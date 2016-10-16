7956ac7475867d7beb810b2aefb3dd4e6e1c7afb
Ezar Enanda <razelot@gmail.com>
2016-10-16 03:30:20 -0700

    stat bars

--- compendium/static/js/main.js
+++ compendium/static/js/main.js
@@ -3,3 +3,17 @@ jQuery(document).ready(function($) {
         window.document.location = $(this).data("href");
     });
 });
+
+function loadStatBar(bar, id, qty){
+  bar = new ProgressBar.Line( id, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1000,
+    color: '#73b246',
+    trailColor: '#888888',
+    trailWidth: 4,
+    svgStyle: {width: '300px', height: '100%'}
+  });
+
+  bar.animate(qty/200);
+}
