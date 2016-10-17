f01df072907590aaa8570ab61e3ea88ee1177a41
Ezar Enanda <razelot@gmail.com>
2016-10-17 01:52:29 -0700

    all stats added

--- compendium/static/js/main.js
+++ compendium/static/js/main.js
@@ -4,15 +4,29 @@ jQuery(document).ready(function($) {
     });
 });
 
-function loadStatBar(bar, id, qty){
-  bar = new ProgressBar.Line( id, {
+function loadStatBar(id, qty){
+  var bar = new ProgressBar.Line( id, {
     strokeWidth: 4,
     easing: 'easeInOut',
     duration: 1000,
     color: '#73b246',
     trailColor: '#888888',
     trailWidth: 4,
-    svgStyle: {width: '300px', height: '100%'}
+    // svgStyle: {width: '300px', height: '100%'}
+  });
+
+  bar.animate(qty/200);
+}
+
+function loadHPBar(id, qty){
+  var bar = new ProgressBar.Line( id, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1000,
+    color: '#FFEB39',
+    trailColor: '#888888',
+    trailWidth: 4,
+    // svgStyle: {width: '300px', height: '100%'}
   });
 
   bar.animate(qty/200);
