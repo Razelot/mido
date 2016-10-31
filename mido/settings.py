45f3670ca01ebdf3c0e6b9eff6e1c34f0c9a5230
Ezar Enanda <razelot@gmail.com>
2016-10-31 00:03:22 -0700

    static files

--- mido/settings.py
+++ mido/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 API_LIMIT_PER_PAGE = 0
