57b2034cec9078dc7fb71a58b540477ff01f4aea
Ezar Enanda <Ezar Enanda>
2016-11-07 02:18:18 -0800

    responsiveness

--- compendium/static/js/main.js
+++ compendium/static/js/main.js
@@ -4,33 +4,7 @@ jQuery(document).ready(function($) {
     });
 });
 
-function loadStatBar(id, qty){
-  var bar = new ProgressBar.Line( id, {
-    strokeWidth: 4,
-    easing: 'easeInOut',
-    duration: 1000,
-    color: '#73b246',
-    trailColor: '#888888',
-    trailWidth: 4,
-    // svgStyle: {width: '300px', height: '100%'}
-  });
-  bar.animate(qty/200);
-}
-
-function loadHPBar(id, qty){
-  var bar = new ProgressBar.Line( id, {
-    strokeWidth: 4,
-    easing: 'easeInOut',
-    duration: 1000,
-    color: '#FFEB39',
-    trailColor: '#888888',
-    trailWidth: 4,
-    // svgStyle: {width: '300px', height: '100%'}
-  });
-  bar.animate(qty/200);
-}
-
-function loaBar(id, qty, color){
+function loadStatBar(id, qty, color){
   var bar = new ProgressBar.Line( id, {
     strokeWidth: 4,
     easing: 'easeInOut',
@@ -38,7 +12,6 @@ function loaBar(id, qty, color){
     color: color,
     trailColor: '#888888',
     trailWidth: 4,
-    // svgStyle: {width: '300px', height: '100%'}
   });
   bar.animate(qty/200);
 }
